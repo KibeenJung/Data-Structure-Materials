412c387e236505f44d00aed3151493ebf49159be
voyager759 <45754143+gbjung759@users.noreply.github.com>
2023-05-15 22:43:43 +0900

    Update list.h

--- linked list/list.h
+++ linked list/list.h
@@ -23,11 +23,17 @@ public:
 		head = nullptr;
 	}
 	~List(void);
+	// appendNode: 리스트의 끝에 value를 가진 노드를 추가한다.
 	Node* appendNode(float value);
+	// insertNode: index 위치에 value를 가진 노드를 삽입한다. 기존의 index 위치에 있던 노드와 그 뒤에 있던 노드들은 한 칸 씩 뒤로 밀린다. index는 0부터 시작한다.
 	Node* insertNode(int index, float value);
+	// removeNode : index 위치에 있는 노드를 삭제한다 (삭제 시 동적해제 필수). index 뒤에 위치해 있던 노드들은 한칸씩 앞으로 밀린다.
 	bool removeNode(int index);
+	// deleteNode : 리스트 앞에서부터 순회하며 value를 가진 노드를 찾는다. 가장 먼저 찾은 노드를 삭제한다.
 	int deleteNode(float value);
+	// findNode : 리스트 앞에서부터 순회하며 value를 가진 노드를 찾고 노드의 인덱스를 반환한다.
 	int findNode(float value);
+	// displayNodes : 리스트를 앞에서부터 순회하며 노드들의 value를 출력한다.
 	void displayNodes(void); 
 };
 
