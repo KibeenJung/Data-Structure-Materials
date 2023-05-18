1affac84057d1ff34e815f314fdf4b39f7738877
voyager759 <45754143+gbjung759@users.noreply.github.com>
2023-05-18 19:59:11 +0900

    Create main.cpp

---
+++ doubly_linked_list/main.cpp
@@ -0,0 +1,92 @@
+/*
+appendNode
+3 2 1 
+insertNode
+3 2 1 4 
+3 2 1 5 4 
+6 3 2 1 5 4 
+7 6 3 2 1 5 4 
+7 8 6 3 2 1 5 4 
+findNode
+find: 0
+find: -1
+find: 7
+find: 2
+find: -1
+7 8 6 3 2 1 5 4 
+removeNode
+remove: 1
+7 8 6 3 2 1 4 
+remove: 1
+7 8 3 2 1 4 
+remove: 0
+7 8 3 2 1 4 
+deleteNode
+delete: 1
+7 3 2 1 4 
+delete: 4
+7 3 2 1 
+delete: 1
+7 2 1 
+1 7 2 1 
+find: 0
+destructor: 2 1 7 2 1
+*/
+
+#include <iostream>
+#include "doubly_list.h"
+
+using namespace std;
+
+int main()
+{
+	DoublyList list;
+	cout << "appendNode" << endl;
+	list.appendHead(2);
+	list.appendHead(1);
+	list.appendTail(3);
+	list.displayNodes();
+
+	cout << "insertNode" << endl;
+	list.insertNode(3, 4);
+	list.displayNodes();
+	list.insertNode(3, 5);
+	list.displayNodes();
+	list.insertNode(10, 6);
+	list.displayNodes();
+	list.insertNode(0, 7);
+	list.displayNodes();
+	list.insertNode(1, 8);
+	list.displayNodes();
+
+	cout << "findNode" << endl;
+	cout << "find: " << list.findNode(7) << endl;
+	cout << "find: " << list.findNode(100) << endl;
+	cout << "find: " << list.findNode(4) << endl;
+	cout << "find: " << list.findNode(6) << endl;
+	cout << "find: " << list.findNode(0) << endl;
+	list.displayNodes();
+
+	cout << "removeNode" << endl;
+	cout << "remove: " << list.removeNode(6) << endl;
+	list.displayNodes();
+	cout << "remove: " << list.removeNode(2) << endl;
+	list.displayNodes();
+	cout << "remove: " << list.removeNode(100) << endl;
+	list.displayNodes();
+
+	cout << "deleteNode" << endl;
+	cout << "delete: " << list.deleteNode(8) << endl;
+	list.displayNodes();
+	cout << "delete: " << list.deleteNode(4) << endl;
+	list.displayNodes();
+	cout << "delete: " << list.deleteNode(3) << endl;
+	list.displayNodes();
+
+	list.appendNode(1);
+	list.displayNodes();
+	cout << "find: " << list.findNode(1) << endl;
+	list.insertNode(0, 2);
+
+	return 0;
+}
