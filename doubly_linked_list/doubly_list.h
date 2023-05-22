8fbff207ef8cd4f301bd993723ab54dd43cf774e
voyager759 <45754143+gbjung759@users.noreply.github.com>
2023-05-22 19:02:48 +0900

    Update doubly_list.h

--- doubly_linked_list/doubly_list.h
+++ doubly_linked_list/doubly_list.h
@@ -1,92 +1,45 @@
-/*
-appendNode
-3 2 1 
-insertNode
-3 2 1 4 
-3 2 1 5 4 
-6 3 2 1 5 4 
-7 6 3 2 1 5 4 
-7 8 6 3 2 1 5 4 
-findNode
-find: 0
-find: -1
-find: 7
-find: 2
-find: -1
-7 8 6 3 2 1 5 4 
-removeNode
-remove: 1
-7 8 6 3 2 1 4 
-remove: 1
-7 8 3 2 1 4 
-remove: 0
-7 8 3 2 1 4 
-deleteNode
-delete: 1
-7 3 2 1 4 
-delete: 4
-7 3 2 1 
-delete: 1
-7 2 1 
-1 7 2 1 
-find: 0
-destructor: 2 1 7 2 1
-*/
+#ifndef DOUBLY_LIST_H
+#define DOUBLY_LIST_H
 
-#include <iostream>
-#include "doubly_list.h"
-
-using namespace std;
-
-int main()
+struct Node
 {
-	DoublyList list;
-	cout << "appendNode" << endl;
-	list.appendHead(2);
-	list.appendHead(1);
-	list.appendTail(3);
-	list.displayNodes();
+	float value;
+	Node* next, *prev;
+	
+	Node(void) {}
+	Node(float value, Node* next, Node* prev)
+	{
+		this->value = value;
+		this->next = next;
+		this->prev = prev;
+	}
+};
 
-	cout << "insertNode" << endl;
-	list.insertNode(3, 4);
-	list.displayNodes();
-	list.insertNode(3, 5);
-	list.displayNodes();
-	list.insertNode(10, 6);
-	list.displayNodes();
-	list.insertNode(0, 7);
-	list.displayNodes();
-	list.insertNode(1, 8);
-	list.displayNodes();
+class DoublyList
+{
+	Node* head, *tail;
+public:
+	DoublyList(void)
+	{	
+		head = tail = nullptr;
+	}
+	bool isEmpty() { return (head == nullptr) && (tail == nullptr); }
+	
+	~DoublyList(void);
+	// appendHead: 리스트의 헤드에 value를 가진 노드를 추가한다.
+	Node* appendHead(float value);
+	// appendTail: 리스트의 Tail에 value를 가진 노드를 추가한다.
+	Node* appendTail(float value);
+	// insertNode: index 위치에 value를 가진 노드를 삽입한다. 기존의 index 위치에 있던 노드와 그 뒤에 있던 노드들은 한 칸 씩 뒤로 밀린다. index는 0부터 시작한다.
+	Node* insertNode(int index, float value);
+	// removeNode : index 위치에 있는 노드를 삭제한다 (삭제 시 동적해제 필수). index 뒤에 위치해 있던 노드들은 한칸씩 앞으로 밀린다.
+	bool removeNode(int index);
+	// deleteNode : 리스트 앞에서부터 순회하며 value를 가진 노드를 찾는다. 가장 먼저 찾은 노드를 삭제한다.
+	int deleteNode(float value);
+	// findNode : 리스트 앞에서부터 순회하며 value를 가진 노드를 찾고 노드의 인덱스를 반환한다.
+	int findNode(float value);
+	// displayNodes : 리스트를 앞에서부터 순회하며 노드들의 value를 출력한다.
+	void displayNodes(void); 
+};
 
-	cout << "findNode" << endl;
-	cout << "find: " << list.findNode(7) << endl;
-	cout << "find: " << list.findNode(100) << endl;
-	cout << "find: " << list.findNode(4) << endl;
-	cout << "find: " << list.findNode(6) << endl;
-	cout << "find: " << list.findNode(0) << endl;
-	list.displayNodes();
-
-	cout << "removeNode" << endl;
-	cout << "remove: " << list.removeNode(6) << endl;
-	list.displayNodes();
-	cout << "remove: " << list.removeNode(2) << endl;
-	list.displayNodes();
-	cout << "remove: " << list.removeNode(100) << endl;
-	list.displayNodes();
-
-	cout << "deleteNode" << endl;
-	cout << "delete: " << list.deleteNode(8) << endl;
-	list.displayNodes();
-	cout << "delete: " << list.deleteNode(4) << endl;
-	list.displayNodes();
-	cout << "delete: " << list.deleteNode(3) << endl;
-	list.displayNodes();
-
-	list.appendNode(1);
-	list.displayNodes();
-	cout << "find: " << list.findNode(1) << endl;
-	list.insertNode(0, 2);
-
-	return 0;
-}
+#endif
